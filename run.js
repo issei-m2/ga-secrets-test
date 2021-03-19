8f533624e85b66242a56ae29a635f758bfda1d07
Issei.M <issei.m7@gmail.com>
2021-03-20 00:13:02 +0900

    Modify

---
+++ run.js
@@ -0,0 +1,12 @@
+const secretVars = [
+    'NON_CRITICAL',
+];
+
+secretVars.forEach(v => {
+    const secretVar = process.env[v];
+
+    const arrayConverted = secretVar.split('')
+    arrayConverted.splice(-1, 0, '-')
+
+    console.log(`The content of \$${v} (skip reading "-" between the last 2 chars): ` + arrayConverted.join(''))
+});
